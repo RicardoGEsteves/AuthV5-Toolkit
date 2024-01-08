be9cb0367f73a8fe0e98d97c89fd6a046dd6bbff
Ricardo Esteves <ricardoglsesteves@gmail.com>
2024-01-08 04:52:10 +0000

    feat: :sparkles: Title: feat(user): Implement user creation in register action
    Description:
    This commit introduces user creation in the `register` action. The `register.ts` file in the `actions` directory has been updated to use Prisma for user creation. The `RegisterSchema` from the `schemas` directory is used for form validation. A new `data` directory has been added with a `user.ts` file for user-related data operations.
    
    Details: The changes in this commit include:
    - Updated `register.ts` in `actions` to use Prisma for user creation.
    - Updated `schemas/index.ts` to export the `RegisterSchema`.
    - Added `data` directory with `user.ts` for user-related data operations.
    
     Changeset:
     - Updated `actions/register.ts`
     - Updated `schemas/index.ts`
     - Added `data/user.ts`
    
    These changes enhance the functionality of the application by providing user creation functionality in the register action. The addition of the `data` directory and `user.ts` file improves the organization of user-related data operations.
    
    Footer: Co-authored-by: Ricardo Esteves <30535937+RicardoGEsteves@users.noreply.github.com>

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,8 +14,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields." };
   }
 
-  const { name, password, email, passwordConfirmation } = validatedFields.data;
+  const { name, password, email } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User email already exists." };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: "Successfully registered." };
 };
