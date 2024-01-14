5d1678b07c8db080d98a74b9773fc879e08f412d
Ricardo Esteves <ricardoglsesteves@gmail.com>
2024-01-14 23:17:04 +0000

    feat: :sparkles: feat(settings-page): Update UI components and add settings page
    Description:
    This commit introduces updates to the user interface components and adds a new settings page. The changes are part of the ongoing work on the `settings-page` branch.
    - In `app/(protected)/_components/navbar.tsx`, `app/(protected)/layout.tsx`, `app/(protected)/settings/page.tsx`, `auth.ts`, `package-lock.json`, `package.json`, and `schemas/index.ts`, updates have been made to handle the UI enhancements and functionality updates.
    - New files `actions/settings.ts`, `components/ui/select.tsx`, `components/ui/sheet.tsx`, `components/ui/switch.tsx`, and `data/account.ts` have been added to handle the new settings page and features.
    
    Changes:
    - Update `app/(protected)/_components/navbar.tsx` to import `MdMenu` from `react-icons/md` for menu icon
    - Update `app/(protected)/layout.tsx`, `app/(protected)/settings/page.tsx`, `auth.ts`, and `schemas/index.ts` for functionality updates
    - Update `package-lock.json` and `package.json` for dependency updates
    - Add new files for handling new settings page and features
    
    This commit doesn't introduce any breaking changes. The updates are compatible with the existing codebase.
    Co-authored-by: Ricardo Esteves <30535937+RicardoGEsteves@users.noreply.github.com>

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -2,31 +2,65 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { MdMenu } from "react-icons/md";
 
 import { NAV_LINKS } from "../_constants";
 import { Button } from "@/components/ui/button";
 import UserButton from "@/components/auth/user-button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const Navbar = () => {
   const pathName = usePathname();
 
   return (
-    // TODO: Mobile navbar
-    <nav className="w-full fixed bg-secondary flex justify-between items-center p-4">
-      <div className="flex gap-x-2">
-        {NAV_LINKS.map((link, index) => (
-          <Button
-            key={index}
-            asChild
-            variant={pathName === link.path ? "default" : "outline"}
-            className="w-full hover:bg-sky-400 hover:text-primary-foreground"
-          >
-            <Link href={link.path}>{link.title}</Link>
-          </Button>
-        ))}
-      </div>
-      <UserButton />
-    </nav>
+    <>
+      <nav className="hidden lg:flex w-full fixed top-0 h-14 bg-secondary justify-between items-center p-4">
+        <div className="flex gap-x-2">
+          {NAV_LINKS.map((link, index) => (
+            <Button
+              key={index}
+              asChild
+              variant={pathName === link.path ? "default" : "outline"}
+              className="w-full hover:bg-sky-400 hover:text-primary-foreground"
+            >
+              <Link href={link.path}>{link.title}</Link>
+            </Button>
+          ))}
+        </div>
+        <UserButton />
+      </nav>
+
+      <nav className="flex bg-secondary items-center justify-between p-4 lg:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Button
+              asChild
+              variant="secondary"
+              size="sm"
+              className="w-full text-background hover:text-sky-400"
+            >
+              <MdMenu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <div className="flex-col p-2">
+              {NAV_LINKS.map((link, index) => (
+                <Button
+                  key={index}
+                  asChild
+                  variant={pathName === link.path ? "default" : "outline"}
+                  className="w-full hover:bg-sky-400 my-2 hover:text-primary-foreground"
+                >
+                  <Link href={link.path}>{link.title}</Link>
+                </Button>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <UserButton />
+      </nav>
+    </>
   );
 };
 export default Navbar;
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,24 +1,236 @@
 "use client";
 
-import { logout } from "@/actions/logout";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTransition, useState } from "react";
+import { useSession } from "next-auth/react";
+import { RiUserSettingsLine } from "react-icons/ri";
+
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { SettingsSchema } from "@/schemas";
+import { settings } from "@/actions/settings";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/password-input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { UserRole } from "@prisma/client";
+import { Switch } from "@/components/ui/switch";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
 
 export default function SettingsPage() {
   const user = useCurrentUser();
 
-  const handleLogout = async () => {
-    await logout();
+  const { update } = useSession();
+
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      password: undefined,
+      newPassword: undefined,
+      newPasswordConfirmation: undefined,
+      name: user?.name || undefined,
+      email: user?.email || undefined,
+      role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof SettingsSchema>) => {
+    try {
+      startTransition(async () => {
+        const data = await settings(values);
+
+        if (data.error) {
+          setError(data.error);
+        }
+
+        if (data.success) {
+          await update();
+          setSuccess(data.success);
+        }
+      });
+    } catch (error) {
+      setError("Something went wrong!");
+    } finally {
+      form.reset();
+      setError("");
+      setSuccess("");
+    }
   };
 
   return (
-    <div>
-      {JSON.stringify(user)}
-      <button
-        onClick={handleLogout}
-        type="submit"
-      >
-        Logout
-      </button>
-    </div>
+    <Card className="w-auto">
+      <CardHeader className="flex-row items-center justify-center font-semibold gap-x-3">
+        <RiUserSettingsLine className="text-sky-400 text-3xl" />
+
+        <p className="text-2xl">Settings</p>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            className="space-y-6"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="John Doe"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {user?.isOAuth === false && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="john.doe@example.com"
+                            type="email"
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="******"
+                            type="password"
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="newPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>New Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="******"
+                            type="password"
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+                    <Select
+                      disabled={isPending}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a role" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+                        <SelectItem value={UserRole.USER}>User</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {user?.isOAuth === false && (
+                <FormField
+                  control={form.control}
+                  name="isTwoFactorEnabled"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                      <div className="space-y-0.5">
+                        <FormLabel>Two Factor Authentication</FormLabel>
+                        <FormDescription>
+                          Enable two factor authentication for your account
+                        </FormDescription>
+                      </div>
+                      <FormControl>
+                        <Switch
+                          disabled={isPending}
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              )}
+            </div>
+            {error && <FormError message={error} />}
+            {success && <FormSuccess message={success} />}
+            <Button
+              disabled={isPending}
+              type="submit"
+              className="w-full hover:bg-sky-400"
+            >
+              Save
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
